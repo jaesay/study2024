da2a7786a33482daf974505dc243edc9e066adb1
Kim Jae Seong <jaesay@ajou.ac.kr>
2024-01-22 19:04:14 +0900

    spring-virtual-thread 프로젝트 생성

---
+++ playground/spring-virtual-thread/src/main/java/com/example/springvirtualthread/SpringVirtualThreadApplication.java
@@ -0,0 +1,13 @@
+package com.example.springvirtualthread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVirtualThreadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringVirtualThreadApplication.class, args);
+    }
+
+}
