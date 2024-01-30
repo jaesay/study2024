a654de9f845856bede149f36ec23d5c6b3793dab
Kim Jae Seong <jaesay@ajou.ac.kr>
2024-01-30 23:10:22 +0900

    Add logging and thread sleep in ProductController, enable virtual threads
    The changes include addition of a logger and thread sleep to the 'getProducts' method in the ProductController. Additionally, the use of virtual threads was activated by setting 'spring.threads.virtual.enabled' to true in application properties.

--- playground/spring-virtual-thread/src/main/java/com/example/springvirtualthread/ProductController.java
+++ playground/spring-virtual-thread/src/main/java/com/example/springvirtualthread/ProductController.java
@@ -1,6 +1,7 @@
 package com.example.springvirtualthread;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +12,15 @@ import java.util.List;
 @RestController
 @RequestMapping("/products")
 @RequiredArgsConstructor
+@Slf4j
 public class ProductController {
 
     private final ProductRepository repository;
 
     @GetMapping
-    public ResponseEntity<List<ProductEntity>> getProducts() {
+    public ResponseEntity<List<ProductEntity>> getProducts() throws InterruptedException {
+        Thread.sleep(1000);
+        log.info("Thread info {}", Thread.currentThread());
         return ResponseEntity.ok(repository.findAll());
     }
 }
