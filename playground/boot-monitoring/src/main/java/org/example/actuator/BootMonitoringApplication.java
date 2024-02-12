6a3c7fad7e097b75c007c1718ed080c5f93e51f6
Kim Jae Seong <jaesay@ajou.ac.kr>
2024-02-12 21:34:14 +0900

    프로젝트명 변경

--- playground/boot-monitoring/src/main/java/org/example/actuator/BootMonitoringApplication.java
+++ playground/boot-monitoring/src/main/java/org/example/actuator/BootMonitoringApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ActuatorApplication {
+public class BootMonitoringApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(ActuatorApplication.class, args);
+    SpringApplication.run(BootMonitoringApplication.class, args);
   }
 
 }
