d9c96e1c6e1dec471eea482e4ca928c63a471f20
Kim Jae Seong <jaesay@ajou.ac.kr>
2024-02-15 22:56:39 +0900

    게이지 메트릭 추가

--- playground/boot-monitoring/src/main/java/org/example/bootmonitoring/BootMonitoringApplication.java
+++ playground/boot-monitoring/src/main/java/org/example/bootmonitoring/BootMonitoringApplication.java
@@ -1,5 +1,7 @@
 package org.example.bootmonitoring;
 
+import org.example.bootmonitoring.order.gauge.StockConfigV1;
+import org.example.bootmonitoring.order.gauge.StockConfigV2;
 import org.example.bootmonitoring.order.v0.OrderConfigV0;
 import org.example.bootmonitoring.order.v1.OrderConfigV1;
 import org.example.bootmonitoring.order.v2.OrderConfigV2;
@@ -9,7 +11,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@Import(OrderConfigV4.class)
+@Import({OrderConfigV4.class, StockConfigV2.class})
 @SpringBootApplication(scanBasePackages = "org.example.bootmonitoring.controller")
 public class BootMonitoringApplication {
 
