abf51a40fa4c56e902feac4b230a36b331765645
Kim Jae Seong <jaesay@ajou.ac.kr>
2024-02-26 23:33:02 +0900

    order domain entity/vo 추가

---
+++ food-ordering-system/order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/OrderItem.java
@@ -0,0 +1,98 @@
+package com.food.ordering.system.order.service.domain.entity;
+
+import com.food.ordering.system.domain.entity.BaseEntity;
+import com.food.ordering.system.domain.valueobject.Money;
+import com.food.ordering.system.domain.valueobject.OrderId;
+import com.food.ordering.system.order.service.domain.valueobject.OrderItemId;
+
+/**
+ * 코어이기 때문에 lombok 종속성 없이 생성 (By innerBuilder plugin)
+ * 생성자 등에서 비즈니스 로직 검사 시에도 장정이 있음..
+ */
+public class OrderItem extends BaseEntity<OrderItemId> {
+
+    private OrderId orderId;
+    private final Product product;
+    private final int quantity;
+    private final Money price;
+    private final Money subTotal;
+
+    private OrderItem(Builder builder) {
+        super.setId(builder.orderItemId);
+        orderId = builder.orderId;
+        product = builder.product;
+        quantity = builder.quantity;
+        price = builder.price;
+        subTotal = builder.subTotal;
+    }
+
+    public OrderId getOrderId() {
+        return orderId;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public Money getPrice() {
+        return price;
+    }
+
+    public Money getSubTotal() {
+        return subTotal;
+    }
+
+    public static final class Builder {
+        private OrderItemId orderItemId;
+        private OrderId orderId;
+        private Product product;
+        private int quantity;
+        private Money price;
+        private Money subTotal;
+
+        private Builder() {
+        }
+
+        public static Builder builder() {
+            return new Builder();
+        }
+
+        public Builder orderItemId(OrderItemId val) {
+            orderItemId = val;
+            return this;
+        }
+
+        public Builder orderId(OrderId val) {
+            orderId = val;
+            return this;
+        }
+
+        public Builder product(Product val) {
+            product = val;
+            return this;
+        }
+
+        public Builder quantity(int val) {
+            quantity = val;
+            return this;
+        }
+
+        public Builder price(Money val) {
+            price = val;
+            return this;
+        }
+
+        public Builder subTotal(Money val) {
+            subTotal = val;
+            return this;
+        }
+
+        public OrderItem build() {
+            return new OrderItem(this);
+        }
+    }
+}
