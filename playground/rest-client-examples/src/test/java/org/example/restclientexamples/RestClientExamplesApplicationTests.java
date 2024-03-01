234429e0ba39d5b51ae3885b3d3025dd8c8daf54
Kim Jae Seong <jaesay@ajou.ac.kr>
2024-03-01 20:20:00 +0900

    rest-client-examples 프로젝트 생성

---
+++ playground/rest-client-examples/src/test/java/org/example/restclientexamples/RestClientExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.restclientexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestClientExamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
