328cba71147c479ac8a3417ede7342b790115ec3
Kim Jae Seong <jaesay@ajou.ac.kr>
2024-03-29 22:43:41 +0900

    예제 애플리케이션(frontend, webapp) 버전업
    - frontend: webapp 요청 에러 발생 시 에러로 응답되도록 변경
    - webapp: 50% 확률로 에러(503) 발생

---
+++ getting-started-with-istio-service-mesh/ch06/webapp/app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+import datetime
+import time
+import threading
+import os
+import random
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    currentDT = datetime.datetime.now()
+    status = 200
+    if random.random() > 0.5:
+        status = 503
+    return "[{}]Welcome user! current time is {} ".format(os.environ['VERSION'],str(currentDT)), status
+
+@app.route("/health")
+def health():
+    return "OK"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
