9252d5006db868690ff7204ca46d4efdcde81ba3
Kim Jae Seong <jaesay@ajou.ac.kr>
2024-05-18 16:24:41 +0900

    spring-helm-example 프로젝트 생성

---
+++ playground/spring-helm-example/src/main/java/org/example/springhelmexample/SpringHelmExampleApplication.java
@@ -0,0 +1,13 @@
+package org.example.springhelmexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHelmExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHelmExampleApplication.class, args);
+    }
+
+}
---
+++ playground/spring-helm-example/src/test/java/org/example/springhelmexample/SpringHelmExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springhelmexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHelmExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
