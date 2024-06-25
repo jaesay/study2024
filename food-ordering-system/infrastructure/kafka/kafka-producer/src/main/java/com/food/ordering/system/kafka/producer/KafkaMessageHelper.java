1b9a872a95475763919dbf9228a8e52bee380436
Kim Jae Seong <jaesay@ajou.ac.kr>
2024-06-25 23:21:50 +0900

    order-messaging: kafka publisher 구현

---
+++ food-ordering-system/infrastructure/kafka/kafka-producer/src/main/java/com/food/ordering/system/kafka/producer/KafkaMessageHelper.java
@@ -0,0 +1,30 @@
+package com.food.ordering.system.kafka.producer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Component;
+
+import java.util.function.BiConsumer;
+
+@Slf4j
+@Component
+public class KafkaMessageHelper {
+
+    public <T> BiConsumer<SendResult<String, T>, Throwable> getKafkaCallback(String responseTopicName, T avroModel, String orderId, String avroModelName) {
+        return (result, ex) -> {
+            if (ex == null) {
+                RecordMetadata metadata = result.getRecordMetadata();
+                log.info("Received successful response from Kafka for order id: {} Topic: {} Partition: {} Offset: {} Timestamp: {}",
+                        orderId,
+                        metadata.topic(),
+                        metadata.partition(),
+                        metadata.offset(),
+                        metadata.timestamp());
+
+            } else {
+                log.error("Error while sending {} message {} to topic {}", avroModelName, avroModel.toString(), responseTopicName, ex);
+            }
+        };
+    }
+}
