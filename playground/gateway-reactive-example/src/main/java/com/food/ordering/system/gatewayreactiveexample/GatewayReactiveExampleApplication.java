850218939784353066055c2c09cf30cb8a72fd6b
Kim Jae Seong <jaesay@ajou.ac.kr>
2024-06-30 20:32:11 +0900

    gateway-reactive-example 프로젝트 추가

---
+++ playground/gateway-reactive-example/src/main/java/com/food/ordering/system/gatewayreactiveexample/GatewayReactiveExampleApplication.java
@@ -0,0 +1,13 @@
+package com.food.ordering.system.gatewayreactiveexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayReactiveExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayReactiveExampleApplication.class, args);
+    }
+
+}
---
+++ playground/gateway-reactive-example/src/test/java/com/food/ordering/system/gatewayreactiveexample/GatewayReactiveExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.food.ordering.system.gatewayreactiveexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayReactiveExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
