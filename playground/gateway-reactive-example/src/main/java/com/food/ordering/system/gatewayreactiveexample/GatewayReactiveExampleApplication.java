a2ec2cc4d1fb3dc3324a6893d06c5b620bb4cc3c
Kim Jae Seong <jaesay@ajou.ac.kr>
2024-06-30 21:41:01 +0900

    gateway-reactive-example 구현

--- playground/gateway-reactive-example/src/main/java/com/food/ordering/system/gatewayreactiveexample/GatewayReactiveExampleApplication.java
+++ playground/gateway-reactive-example/src/main/java/com/food/ordering/system/gatewayreactiveexample/GatewayReactiveExampleApplication.java
@@ -2,6 +2,9 @@ package com.food.ordering.system.gatewayreactiveexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayReactiveExampleApplication {
@@ -9,5 +12,25 @@ public class GatewayReactiveExampleApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayReactiveExampleApplication.class, args);
     }
+
+//    @Bean
+//    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+//        return builder.routes()
+//                .route(r -> r.path("/posts/**")
+//                        .filters(f -> f
+//                                .prefixPath("/api")
+//                                .addResponseHeader("X-Powered-By", "Gateway Reactive Example")
+//                        )
+//                        .uri("http://localhost:8081")
+//                )
+//                .route(r -> r.path("/comments/**")
+//                        .filters(f -> f
+//                                .prefixPath("/api")
+//                                .addResponseHeader("X-Powered-By", "Gateway Reactive Example")
+//                        )
+//                        .uri("http://localhost:8082")
+//                )
+//                .build();
+//    }
 
 }
