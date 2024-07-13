13261c97ca9931b0d10784b20d4c7f5cbae5fb9e
Kim Jae Seong <jaesay@ajou.ac.kr>
2024-07-13 22:55:38 +0900

    spring-boot-k8s-example 프로젝트 생성

---
+++ playground/spring-boot-k8s-example/src/main/java/com/food/ordering/system/springbootk8sexample/SpringBootK8sExampleApplication.java
@@ -0,0 +1,13 @@
+package com.food.ordering.system.springbootk8sexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootK8sExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootK8sExampleApplication.class, args);
+    }
+
+}
---
+++ playground/spring-boot-k8s-example/src/test/java/com/food/ordering/system/springbootk8sexample/SpringBootK8sExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.food.ordering.system.springbootk8sexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootK8sExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
